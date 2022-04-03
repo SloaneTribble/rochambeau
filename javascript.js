df43b7b2d71f0230f2fe17f1c80a63c4651a094f
Sloane Tribble <SloaneTribble@gmail.com>
2022-04-03 10:59:56 -0700

    Add three buttons with event listeners

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-// 1 = Rock, 2 = Paper, 3 = Scissors
 
 function computerPlay(){
     let choice = Math.floor(Math.random()*3) + 1;
@@ -15,78 +14,44 @@ function computerPlay(){
     return choice;
 }
 
-function singleRound(playerSelection, computerPlay){
-    let human = playerSelection.toLowerCase();
+function singleRound(playerSelection){
+    let human = playerSelection
     let computer = computerPlay();
-    console.log("You chose: " + human + ", computer chose: " + computer);
+    let result = "";
     switch (true){
         case (human === computer):
-            return "Tie game!";
+            result =  "Tie game!";
             break;
         case (human === "rock" && computer === "paper"):
-            return "You lose!";
+            result = "You lose!";
             break;
         case (human === "rock" && computer === "scissors"):
-            return "You win!";
+            result = "You win!";
             break;
         case (human === "paper" && computer === "rock"):
-            return "You win!";
+            result = "You win!";
             break;
         case (human === "paper" && computer === "scissors"):
-            return "You lose!";
+            result = "You lose!";
             break;
         case (human === "scissors" && computer === "paper"):
-            return "You win!";
+            result = "You win!";
             break;
         case (human === "scissors" && computer === "rock"):
-            return "You lose!";
+            result = "You lose!";
             break;
         default: 
             alert("Please enter rock, paper or scissors");
             break;
     }
+    return "You chose: " + human + ", computer chose: " + computer + ". " + result;
 }
 
-function game(){
-    alert("You are about to play Rochambeau's Revenge, best of five.");
-    let computerScore = 0;
-    let humanScore = 0;
-    for(let i = 0; i < 5; i++){
-        let playerSelection = prompt("Rock, paper or scissors?");
-        let roundResult = singleRound(playerSelection, computerPlay);
-        switch(roundResult){
-            case "Tie game!":
-                alert(`That one was a tie! You: ` + humanScore 
-                + `; Computer: ` + computerScore);
-                break;
-            case "You win!": 
-                humanScore += 1;
-                alert(`You won that one! You: ` + humanScore
-                + `; Computer: ` + computerScore);
-                break;
-            case "You lose!":
-                computerScore += 1;
-                alert(`Dang it! That was a loss. You: ` + humanScore
-                + `; Computer: ` + computerScore);
-                break;
-            default: 
-                alert("That round was botched.  Time for a re-do!");
-                i -= 1;
-                break;
-        }
-    
-    }
-    switch(true){
-        case (humanScore === computerScore):
-            alert("In the end, it was a tie.");
-            break;
-        case (humanScore > computerScore):
-            alert("You are the victor!");
-            break;
-        case (humanScore < computerScore):
-            alert("Dang it, better luck next time.");
-            break;
-    }
-}
+const buttons = document.querySelectorAll('button');
 
-game();
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerChoice = button.className;
+        alert(singleRound(playerChoice));
+    });
+});
